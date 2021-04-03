4e3bd9bb8cfbf389647496b6d3b45bfa10278522
AdmiringWorm <kim.nordmo@gmail.com>
2021-04-03 18:44:10 +0200

    feat(web): add ability to output checksum of downloaded file

--- pkg-upd/src/bin/pkg-web.rs
+++ pkg-upd/src/bin/pkg-web.rs
@@ -3,20 +3,60 @@
 #![windows_subsystem = "console"]
 
 use std::fmt::Display;
+use std::io::Write;
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
 
 use human_bytes::human_bytes;
 use human_panic::setup_panic;
 use log::{error, info};
+use md5::Md5;
 use pkg_upd::{log_data, logging};
 use pkg_web::errors::WebError;
 use pkg_web::response::ResponseType;
 use pkg_web::{LinkElement, WebRequest, WebResponse};
+use sha1::Sha1;
+use sha2::{Digest, Sha256, Sha512};
 use structopt::StructOpt;
 use url::Url;
 use yansi::Color;
 
 log_data! {"pkg-web"}
+
+#[derive(StructOpt)]
+enum ChecksumType {
+    Md5,
+    Sha1,
+    Sha256,
+    Sha512,
+}
+
+impl FromStr for ChecksumType {
+    type Err = &'static str;
+
+    fn from_str(val: &str) -> std::result::Result<Self, <Self as std::str::FromStr>::Err> {
+        let val: &str = &val.trim().to_lowercase();
+
+        match val {
+            "md5" => Ok(ChecksumType::Md5),
+            "sha1" => Ok(ChecksumType::Sha1),
+            "sha256" | "sha2" => Ok(ChecksumType::Sha256),
+            "sha512" => Ok(ChecksumType::Sha512),
+            _ => Err("The value is not a supported checksum type"),
+        }
+    }
+}
+
+impl Display for ChecksumType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        match self {
+            ChecksumType::Md5 => f.write_str("MD5"),
+            ChecksumType::Sha1 => f.write_str("SHA1"),
+            ChecksumType::Sha256 => f.write_str("SHA256"),
+            ChecksumType::Sha512 => f.write_str("SHA512"),
+        }
+    }
+}
 
 #[derive(StructOpt)]
 #[structopt(after_help = "EXAMPLES:
@@ -51,6 +91,14 @@ struct DownloadArguments {
     /// previously was returned from the server.
     #[structopt(long)]
     last_modified: Option<String>,
+
+    /// The checksum to compare the downloaded file with
+    #[structopt(long)]
+    checksum: Option<String>,
+
+    /// The type of the checksum to compare and/or output to console.
+    #[structopt(long, default_value = "sha256", possible_values = &["md5", "sha1", "sha256", "sha512"])]
+    checksum_type: ChecksumType,
 
     /// The work directory that downloads should be downloaded to. [default:
     /// temp dir]
@@ -98,19 +146,7 @@ fn main() {
     let request = WebRequest::create();
     match args.cmd {
         Commands::Parse(args) => parse_website_lone(&request, args.url, args.regex),
-        Commands::Download(args) => {
-            let etag = if let Some(ref etag) = args.etag {
-                Some(etag.as_str())
-            } else {
-                None
-            };
-            let last_modified = if let Some(ref last_modified) = args.last_modified {
-                Some(last_modified.as_ref())
-            } else {
-                None
-            };
-            download_file_once(&request, args.url, etag, last_modified, args.work_dir)
-        }
+        Commands::Download(args) => download_file_once(&request, args),
     }
 }
 
@@ -167,20 +203,14 @@ fn parse_website(
     }
 }
 
-fn download_file_once(
-    request: &WebRequest,
-    url: Url,
-    etag: Option<&str>,
-    last_modified: Option<&str>,
-    work_dir: Option<PathBuf>,
-) {
-    let temp_dir = if let Some(work_dir) = work_dir {
-        work_dir
+fn download_file_once(request: &WebRequest, args: DownloadArguments) {
+    let temp_dir = if let Some(work_dir) = &args.work_dir {
+        work_dir.clone()
     } else {
         std::env::temp_dir()
     };
 
-    if let Err(err) = download_file(request, url, &temp_dir, etag, last_modified) {
+    if let Err(err) = download_file(request, args, &temp_dir) {
         error!("Unable to download the file. Error: {}", err);
         std::process::exit(1);
     }
@@ -188,12 +218,20 @@ fn download_file_once(
 
 fn download_file(
     request: &WebRequest,
-    url: Url,
+    args: DownloadArguments,
     work_dir: &Path,
-    etag: Option<&str>,
-    last_modified: Option<&str>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let response = request.get_binary_response(url.as_str(), etag, last_modified)?;
+    let etag = if let Some(ref etag) = args.etag {
+        Some(etag.as_str())
+    } else {
+        None
+    };
+    let last_modified = if let Some(ref last_modified) = args.last_modified {
+        Some(last_modified.as_str())
+    } else {
+        None
+    };
+    let response = request.get_binary_response(args.url.as_str(), etag, last_modified)?;
 
     match response {
         ResponseType::Updated(_) => {
@@ -214,6 +252,21 @@ fn download_file(
             } else {
                 print_line("Last Modified", "None");
             }
+            match generate_checksum(&result, &args.checksum_type) {
+                Ok(checksum) => {
+                    if let Some(original_checksum) = args.checksum {
+                        if original_checksum.to_lowercase() == checksum {
+                            info!("Originial Checksum matches checksum of downloaded file");
+                        } else {
+                            error!("Original Checksum do not match checksum of downloaded file!");
+                        }
+                    }
+                    print_line("Checksum", checksum);
+                    print_line("Checksum Type", args.checksum_type);
+                }
+                Err(err) => error!("Unable to generate checksum: {}", err),
+            }
+
             info!(
                 "The resulting file is {} long!",
                 Color::Cyan.paint(human_bytes(result.metadata()?.len() as f64))
@@ -223,6 +276,31 @@ fn download_file(
         }
     }
     Ok(())
+}
+
+fn generate_checksum(path: &Path, checksum_type: &ChecksumType) -> Result<String, std::io::Error> {
+    match checksum_type {
+        ChecksumType::Md5 => generate_checksum_from_hasher(Md5::new(), path),
+        ChecksumType::Sha1 => generate_checksum_from_hasher(Sha1::new(), path),
+        ChecksumType::Sha256 => generate_checksum_from_hasher(Sha256::new(), path),
+        ChecksumType::Sha512 => generate_checksum_from_hasher(Sha512::new(), path),
+    }
+}
+
+fn generate_checksum_from_hasher<T: Digest + Write>(
+    mut hasher: T,
+    path: &Path,
+) -> Result<String, std::io::Error>
+where
+    <T as sha2::Digest>::OutputSize: std::ops::Add,
+    <<T as sha2::Digest>::OutputSize as std::ops::Add>::Output:
+        sha2::digest::generic_array::ArrayLength<u8>,
+{
+    let mut f = std::fs::File::open(path)?;
+    std::io::copy(&mut f, &mut hasher)?;
+    let result = hasher.finalize();
+
+    Ok(format!("{:x}", result))
 }
 
 fn print_line<T: Display, V: Display>(name: T, value: V) {
